265930e3cc2fabd4d7d051e263e8a8bbbbc1aa1e
KirillDominov <spotik321@gmail.com>
2025-01-24 23:13:54 +0500

    Donev3

--- src/main/java/skypro/calculator/service/CalculatorServiceImpl.java
+++ src/main/java/skypro/calculator/service/CalculatorServiceImpl.java
@@ -24,16 +24,25 @@ public class CalculatorServiceImpl implements CalculatorService {
 
     @Override
     public int getMinus(int num1, int num2) {
+        if (num1==null || num2==null){
+            throw new NotEnoughArgumentException("один из двух параметров (или оба) не введены")
+        }
         return num1 - num2;
     }
 
     @Override
     public int getMultiply(int num1, int num2) {
+        if (num1==null || num2==null){
+            throw new NotEnoughArgumentException("один из двух параметров (или оба) не введены")
+        }
         return num1 * num2;
     }
 
     @Override
     public int getDivide(int num1, int num2) {
+        if (num1==null || num2==null){
+            throw new NotEnoughArgumentException("один из двух параметров (или оба) не введены")
+        }
         if (num2 != 0) {
             return num1 / num2;
         } else {
